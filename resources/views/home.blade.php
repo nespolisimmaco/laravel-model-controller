c4b3ca2841bceff7b63cce4e2e26d459173ed134
nespolisimmaco <snespoli@yahoo.com>
2023-05-24 19:01:40 +0200

    Add: created cards and added style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,31 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
-    <main>
-        
-    </main>
-
-</body>
-
-</html>
+@section('content')
+{{-- Container --}}
+<div class="container">
+    {{-- Row --}}
+    <div class="row row-cols-4 g-4">
+        {{-- For each --}}
+        @foreach ($movies as $movie)
+        {{-- Columns --}}
+        <div class="col">
+            {{-- Card --}}
+            <div class="card h-100 border-0" style="width: 18rem;">
+                <img src="{{ $movie['image'] }}" class="card-img-top" alt="{{ $movie['title'] }}">
+                <div class="card-body">
+                    <div class="title"><h5>Title: {{ $movie['title'] }}</h5></div>
+                    <div class="original-title">Original title: {{ $movie['original_title'] }}</div>
+                    <div class="nationality">Nationality: {{ $movie['nationality'] }}</div>
+                    <div class="rating">Rating: {{ $movie['vote'] }}</div>
+                </div>
+            </div>
+            {{-- End Card --}}
+        </div>
+        {{-- Columns --}}
+        @endforeach
+        {{-- End For each --}}
+    </div>
+    {{-- End Row --}}
+</div>
+{{-- End Container --}}
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header class="d-flex justify-content-center align-items-center">
+    <div>
+        <img src="{{ Vite::asset('resources/img/Boolflix_logo.png') }}" alt="Boolflix logo">
+    </div>
+</header>
